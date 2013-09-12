6e60d31f0024cdabce7eaa02ff49f29941f775a6
Mims H. Wright <mwright@hugeinc.com>
2013-09-12 08:28:01 -0700

    cleaned up grunt file and demo build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
 		htmlPath: 'src/html/',
 		deployPath: 'deploy/',
 		testPath: 'test/',
+		demoPath: 'demo/',
 
 		coffeeFiles : [{
 								expand: true,
@@ -60,52 +61,58 @@ module.exports = function(grunt) {
 			}
 		},
 
-		copy: {
-			html: {
-				files: [{ 
-                    src: [ '**/*.html' ],
-                    dest: '<%= deployPath %>/demo/',
-                    cwd: '<%= htmlPath %>',
-                    expand: true
-                }]   
-			}
-		},
-
 		qunit: {
 				files: ['<%=testPath%>**/*.html']
 		},
 
-		shell: {
-			openTestsInBrowser: {
-				command: 'open <%=testPath%>index.html'
-			},
-			openDemo: {
-				command: 'open <%= deployPath %>demo/index.html'
-			}
-		},
+		open: {
+            test : {
+              path: '<%=testPath%>/index.html',
+              app: 'Google Chrome'
+            },
+            demo : {
+              path: '<%=demoPath%>/index.html',
+              app: 'Google Chrome'
+            }
+        },
 
 		watch: {
 			coffee: {
 				files: '<%=coffeeSrcPath%>**/*.coffee',
 				tasks: ['lint', 'coffee:dev'],
 			}
-		}
+		},
+
+		bump: {
+          options: {
+            files: ['package.json'],
+            updateConfigs: [],
+            commit: true,
+            commitMessage: 'Version v%VERSION%',
+            commitFiles: ['package.json'], // '-a' for all files
+            createTag: true,
+            tagName: 'v%VERSION%',
+            tagMessage: 'Version %VERSION%',
+            push: false
+          }
+        }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-coffeelint');
-	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-open');
 
-	grunt.registerTask('default', ['lint', 'coffee:dev', 'qunit']);
+	grunt.registerTask('default', ['dev']);
+	grunt.registerTask('dev', ['lint', 'coffee:dev', 'qunit']);
 	grunt.registerTask('lint', ['coffeelint']);
-	grunt.registerTask('test', ['lint', 'coffee:dev', 'shell:openTestsInBrowser']);
+	grunt.registerTask('test', ['lint', 'coffee:dev', 'open:test']);
+	grunt.registerTask('demo', ['deploy', 'open:demo']);
 	grunt.registerTask('deploy', ['clean', 'lint', 'coffee:deploy', 'uglify:deploy', 'qunit']);
 
-	grunt.registerTask('demo', ['deploy', 'copy:html', 'shell:openDemo']);
 };
