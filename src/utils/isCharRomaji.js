350b28ae14b76da0c882fdab43d74ba82f5061ac
Duncan Bay <duncan.bay@gmail.com>
2017-08-28 12:53:27 +0800

    feat: add more unicode ranges, extend Japanese char ranges
    Closes #63

--- src/utils/isCharRomaji.js
+++ src/utils/isCharRomaji.js
@@ -1,14 +1,15 @@
 import isEmpty from './isEmpty';
-import { ROMAJI_REGEX } from '../constants';
+import isCharInRange from './isCharInRange';
+import { ROMAJI_RANGES } from '../constants';
 
 /**
- * Tests a character. Returns true if the character is [Romaji](https://en.wikipedia.org/wiki/Romaji)
+ * Tests a character. Returns true if the character is [Romaji](https://en.wikipedia.org/wiki/Romaji) (allowing [Hepburn romanisation](https://en.wikipedia.org/wiki/Hepburn_romanization))
  * @param  {String} char character string to test
  * @return {Boolean}
  */
-function isCharKana(char = '') {
+function isCharRomaji(char = '') {
   if (isEmpty(char)) return false;
-  return ROMAJI_REGEX.test(char);
+  return ROMAJI_RANGES.some(([start, end]) => isCharInRange(char, start, end));
 }
 
-export default isCharKana;
+export default isCharRomaji;
