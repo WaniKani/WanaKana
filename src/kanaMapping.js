21a0de013d2ab9d33db694f76aad5e9b9a877ed4
Geggles <geggles@live.de>
2017-11-02 11:04:05 +0100

    Create the new applyMapping function that parses a string with a given mapping, without making any assumptions about IMEMode etc.

--- src/kanaMapping.js
+++ src/kanaMapping.js
@@ -1,11 +1,54 @@
+import isEmpty from "./utils/isEmpty";
+
 export function IME_MODE_MAP(map) {
   // in IME mode, we do not want to convert single ns
   const mapCopy = JSON.parse(JSON.stringify(map));
-  // two typed ns should produce a single ん in IME mode
-  delete mapCopy.n[''];
   mapCopy.n.n = { '': 'ん' };
   mapCopy.n[' '] = { '': 'ん' };
   return mapCopy;
+}
+
+export function applyMapping(string, mapping, convertEnding=true) {
+  const root = mapping;
+
+  function nextSubtree(tree, nextChar) {
+    const subtree = tree[nextChar];
+    if (subtree === undefined) {
+      return undefined;
+    }
+    // if the next child node does not have a node value, set its node value to the input
+    return Object.assign({ '': tree[''] + nextChar }, tree[nextChar]);
+  }
+
+  function newChunk(remaining, currentCursor) {
+    // start parsing a new chunk
+    const firstChar = remaining.charAt(0);
+    return parse(Object.assign({ '': firstChar }, root[firstChar]), remaining.slice(1), currentCursor, currentCursor+1);
+  }
+
+  function parse(tree, remaining, lastCursor, currentCursor) {
+    if (!remaining) {
+      if (convertEnding || Object.keys(tree).length === 1) {  // nothing more to consume, just commit the last chunk and return it
+        // so as to not have an empty element at the end of the result
+        return tree['']? [[lastCursor, currentCursor, tree['']]]: [];
+      }
+      // if we don't want to convert the ending, because there are still possible continuations left, just return null as the final node value
+      return [[lastCursor, currentCursor, null]];
+    }
+
+    if (Object.keys(tree).length === 1) {
+      return [[lastCursor, currentCursor, tree['']]].concat(newChunk(remaining, currentCursor));
+    }
+
+    const subtree = nextSubtree(tree, remaining.charAt(0));
+    if (subtree === undefined) {
+      return [[lastCursor, currentCursor, tree['']]].concat(newChunk(remaining, currentCursor));
+    }
+
+    // continue current branch
+    return parse(subtree, remaining.slice(1), lastCursor, currentCursor+1);
+  }
+  return newChunk(string, 0);
 }
 
 export const USE_OBSOLETE_KANA_MAP = createCustomRomajiToKanaMapping({ wi: 'ゐ', we: 'ゑ' });
@@ -68,6 +111,7 @@ export function createCustomRomajiToKanaMapping(customMap) {
 function createKanaToHepburnMap() {
   /* eslint object-property-newline: ["error", { "allowMultiplePropertiesPerLine": true }] */
   const hepburnTree = {
+    '': '',
     あ: 'a',
     い: 'i',
     う: 'u',
