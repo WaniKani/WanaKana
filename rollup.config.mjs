92d351fc74069c274a88e4375962ea20716db121
Tabitha Larkin <tabithalarkin.me@gmail.com>
2023-11-08 22:47:58 +0900

    build(rollup): fix version compatibility with deprecated terser plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json' assert { type: "json" };
 import ts from "rollup-plugin-ts";
